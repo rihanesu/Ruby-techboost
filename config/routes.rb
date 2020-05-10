2f1ba583520a8872e2f0debda7804ad29a7a33f8
EC2 Default User <ec2-user@ip-172-31-17-90.us-east-2.compute.internal>
2020-05-10 03:04:20 +0000

    Ruby Rails 05 課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'pages#index'
   get 'pages/help'
   
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destory'
 end
